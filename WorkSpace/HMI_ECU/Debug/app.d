956505f164a362140705de07d5366298231c0ebe
Ahmad Haroun <109513775+Ahmad00Ragab@users.noreply.github.com>
2023-09-28 15:31:17 +0300

    Add files via upload

---
+++ WorkSpace/HMI_ECU/Debug/app.d
@@ -0,0 +1,18 @@
+app.o app.o: ../app.c ../app.h ../std_types.h ../lcd.h ../uart.h \
+  ../gpio.h ../common_macros.h ../keypad.h ../timer1.h
+
+../app.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../uart.h:
+
+../gpio.h:
+
+../common_macros.h:
+
+../keypad.h:
+
+../timer1.h:
